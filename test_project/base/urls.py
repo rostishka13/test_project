a64ff340b1ace586eda0b84af3a7acde4c9bc357
Rostyk <rostishka1@gmail.com>
2021-06-08 09:45:52 +0300

    addind group and users template

--- test_project/base/urls.py
+++ test_project/base/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import  home
+from .views import  HomeView,UsersView, GroupView
 
 urlpatterns = [
-    path('', home,  name='home'),
-    
+    path('', HomeView.as_view(),  name='home'),
+    path('users/',UsersView.as_view(), name='users'),
+    path('groups/', GroupView.as_view() , name='groups'),
+
     
 ]
--- test_project/base/views.py
+++ test_project/base/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render, HttpResponse
+
+from .models import Groups, List_of_Users
+from django.views.generic import ListView, CreateView, DeleteView
 
 # Create your views here.
-def home(request):
-    return HttpResponse('hi')
+class HomeView(ListView):
+    model = List_of_Users
+    template_name = 'base/home.html'
+class UsersView(ListView):
+    model = List_of_Users
+    context_object_name = 'users'
+    template_name = 'base/users.html'
+class GroupView(ListView):
+    model = List_of_Users
+    template_name = 'base/groups.html'
+    context_object_name = 'groups'
